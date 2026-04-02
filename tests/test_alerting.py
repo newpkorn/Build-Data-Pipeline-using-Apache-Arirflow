3187233f98ab5b9a22a26ca41c875156bb7d0daf
pakorn <pakorn.soikham@gmail.com>
2026-04-02 15:55:40 +0700

    fix(tests): remove global airflow module monkeypatch in test_alerting

--- tests/test_alerting.py
+++ tests/test_alerting.py
@@ -1,23 +1,5 @@
-import sys
-import types
 from types import SimpleNamespace
 from unittest.mock import patch
-
-
-airflow_module = types.ModuleType("airflow")
-airflow_models_module = types.ModuleType("airflow.models")
-
-
-class DummyVariable:
-    @staticmethod
-    def get(name):
-        raise NotImplementedError
-
-
-airflow_models_module.Variable = DummyVariable
-airflow_module.models = airflow_models_module
-sys.modules.setdefault("airflow", airflow_module)
-sys.modules.setdefault("airflow.models", airflow_models_module)
 
 from utils.alerting import discord_alert
 
@@ -48,5 +30,5 @@ def test_discord_alert_posts_on_terminal_failure(mock_variable_get, mock_post):
 
     discord_alert(context)
 
-    mock_variable_get.assert_called_once_with("discord_webhook")
+    mock_variable_get.assert_called_once_with("discord_webhook", default_var=None)
     mock_post.assert_called_once()
